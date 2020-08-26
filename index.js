37c5d715105ea48178653dcd5407e63c59e5988a
pairofAces <karanchauhan16@gmail.com>
2020-08-25 23:18:32 -0400

    finished

--- index.js
+++ index.js
@@ -40,7 +40,57 @@ function toggleColor(element) {
   }
 }
 
+const header = document.querySelector('#header')
+header.addEventListener("click", function(){
+  toggleColor(header)
+});
+
 
 /***** Deliverable 2 *****/
 
+// 1. I need to get the form
+// 2. I need to create the listener for the form and attach a callback function that reacts to the listener
+
+const form = document.querySelector("#new-player-form")
+form.addEventListener("submit", formSubmit)
+
+// 3. create the callback function being referenced
+
+function formSubmit(event) {
+  event.preventDefault()
+
+        // creating a 'form' variable to access the form input
+  const form = event.target
+
+        // creating a variable for the new player so I can add the info into the DOM
+  const newPlayer = {
+    number: form["number"].value,
+    name: form["name"].value,
+    nickname: form["nickname"].value,
+    photo: form["photo"].value,
+    likes: 1000
+  }
+
+  // now I need to create the player with the the 'newPlayer' variable
+  renderPlayer(newPlayer)
+}
+
+
+
 /***** Deliverable 3 *****/
+
+// use the 'playerContainer' variable created at the 'starter-code' section of this file. 
+//    - then create a listener for the 'like click'
+//        - can use the '.matches' method to look for the 'click' on the 'like' button 
+//            - from the console, I can see that the button is labeled as a class "like-button" 
+//                - I can use dot notation to access it
+
+playerContainer.addEventListener("click", function(e){
+  if (e.target.matches(".like-button")) {
+    const player = e.target.closest(".player") // find the first parent element of the player who's like button was clicked
+    
+    const likeAmount = player.querySelector(".likes")  // find the element that I want to update
+
+    likeAmount.textContent = `${parseInt(likeAmount.textContent) + 1} likes`    // update the element's 'textContent' 
+  }
+})
